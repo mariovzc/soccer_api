a8c16f39e5dc78382616d39a54e651e546dcf9dd
¨Mario <mario.vizcainos@gmail.com>
2017-10-27 09:24:44 -0500

    setup token validation method

--- app/routes.js
+++ app/routes.js
@@ -10,8 +10,32 @@ module.exports = function (app, express) {
 
   // get an instance of the router for api routes
   let apiRoutes = express.Router()
-  // TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
-  // TODO: route middleware to verify a token
+
+  // route middleware to verify a token
+  function isLoggedIn (req, res, next) {
+      // check header or url parameters or post parameters for token
+    const token = req.body.token || req.query.token || req.headers['x-access-token']
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' })
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded
+          next()
+        }
+      })
+    } else {
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      })
+    }
+  }
 
   // route to show a random message (GET http://localhost:8080/api/)
   apiRoutes.get('/', function (req, res) {
@@ -32,7 +56,7 @@ module.exports = function (app, express) {
   })
 
   // route to return all users (GET http://localhost:8080/api/users)
-  apiRoutes.get('/users', function (req, res) {
+  apiRoutes.get('/users', isLoggedIn, function (req, res) {
     User.find({}, function (err, users) {
       res.json(users)
     })

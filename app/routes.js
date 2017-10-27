ed14d8d6ffbf379f72b4ba6c19a58d092118d8d5
¨Mario <mario.vizcainos@gmail.com>
2017-10-27 09:16:58 -0500

    added create user route

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app, express) {
-  var User = require('./models/user') // get our mongoose model
-  var jwt = require('jsonwebtoken') // used to create, sign, and verify tokens  
+  let User = require('./models/user') // get our mongoose model
+  let jwt = require('jsonwebtoken') // used to create, sign, and verify tokens
   // =======================
   // routes ================
   // =======================
@@ -9,7 +9,7 @@ module.exports = function (app, express) {
   // we'll get to these in a second
 
   // get an instance of the router for api routes
-  var apiRoutes = express.Router()
+  let apiRoutes = express.Router()
   // TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
   // TODO: route middleware to verify a token
 
@@ -18,10 +18,51 @@ module.exports = function (app, express) {
     res.json({ message: 'Welcome to the coolest API on earth!' })
   })
 
+  apiRoutes.post('/user/new', function (req, res) {
+    const newUser = new User({
+      name: req.body.name,
+      password: req.body.password,
+      leage: req.body.leage
+    })
+    newUser.save(function (err) {
+      if (err) throw err
+      console.log('User saved successfully')
+      res.json({ success: true })
+    })
+  })
+
   // route to return all users (GET http://localhost:8080/api/users)
   apiRoutes.get('/users', function (req, res) {
     User.find({}, function (err, users) {
       res.json(users)
+    })
+  })
+  //  AUTHENTICATE USER
+  apiRoutes.post('/authenticate', function (req, res) {
+    // find the user
+    User.findOne({
+      name: req.body.name
+    }, function (err, user) {
+      if (err) throw err
+      if (!user) {
+        res.json({ success: false, message: 'Authentication failed. User not found.' })
+      } else if (user) {
+        // check if password matches
+        if (user.password !== req.body.password) {
+          res.json({ success: false, message: 'Authentication failed. Wrong password.' })
+        } else {
+          // if user is found and password is right
+          // create a token with only our given payload
+          // we don't want to pass in the entire user since that has the password
+          var token = jwt.sign(toString('hex'), app.get('superSecret'))
+          // return the information including token as JSON
+          res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token
+          })
+        }
+      }
     })
   })
   app.use('/api', apiRoutes)
